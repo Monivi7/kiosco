5466a05d0097a6d943534eeb9b241f1d381ef297
¨Moni¨ <monicamadalonhotmail.com>
2025-05-29 17:28:45 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {  
+    
+    return ( 
+        <header style={{ backgroundColor: "#4CAF50", padding: "10px", textAlign: "center", color: "white" }}>  
+           Bienvenidos a la casa de las golosinas
+        </header>  
+    );  
+}  
+
+export default Header;
